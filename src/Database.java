8b052f5a666c5ecae20cf9d86fc6bfc4af71a753
kylehoffpauir <kyle@hoffpauir.com>
2022-10-27 23:22:39 -0400

    A-D DONE (more or less) A) creates table with primary key B) inserts into table, requires every value C) removes from table based on a single user inputted column, val pair D) prints the table, albiet skrunkly

--- src/Database.java
+++ src/Database.java
@@ -102,66 +102,80 @@ public class Database {
                 System.err.println("Error! cannot create table " + tableName + " as it already exists!");
                 return;
             }
+            ArrayList<String> variables = new ArrayList<String>();
             //take in and store column info
+            System.out.println("Reading variables - to continue, simply hit enter on all three fields");
             while(keepReading) {
-                System.out.print("Enter column name and it's length: ");
-                String line = in.nextLine();
-                System.out.println();
-                if (line.equals(""))
+                String var = getVariableInformation(in);
+                if(!var.equals(""))
+                    variables.add(var);
+                else
                     keepReading = false;
-                else {
-                    String[] lineSplit = line.split(" ");
-                    String name = lineSplit[0];
-                    int size = Integer.parseInt(line.split(" ")[1]);
-                    // ensure single word colNames
-                    if (name.contains(" ")) {
-                        System.err.println("Error! Db columns must be single words");
-                        table.delete();
-                        System.err.println("Table deleted.");
-                        return;
-                    }
-                    // add to lists
-                    while (name.length() - 1 != size)
-                        name += " ";
-                    colNames.add(name);
-                    colLengths.add(size);
-                }
             }
+            //build string for create
+            String sqlCreate = "CREATE TABLE " + tableName + " (";
+            for(int i = 0; i < variables.size()-1; i++) {
+                sqlCreate += variables.get(i) + ", ";
+            }
+            sqlCreate += variables.get(variables.size()-1);
+
+            System.out.println("Specify primary key? (Y / N)");
+            String option = in.nextLine();
+            // if we have a primary key add the , PRIMARY KEY (var))
+            switch(option.toUpperCase()){
+                case "Y":
+                    System.out.println("Enter the variable would you like to set as the primary key:");
+                    String key = in.nextLine();
+                    sqlCreate += ", PRIMARY KEY ( " + key + " ))";
+                    break;
+                case "N":
+                default:
+                    sqlCreate += ")";
+                    break;
+            }
+            //send create statement to the DB
+            stmt.executeUpdate(sqlCreate);
         } catch (Exception e) {
                 System.err.println("ERROR - error creating table");
+                e.printStackTrace();
                 return;
         }
-        System.out.println("Done collecting!");
-        //save column and table info to disk
-        try {
-            //dump(table, colNames, colLengths);
-            //TODO write to db
-            System.out.println("Wrote to disk!");
-        } catch(Exception e) {
-            System.err.println("Error writing to disk. Table not saved.");
-            if(DEBUG)
-                System.err.println(e.toString());
-        }
-
+        System.out.println("Wrote table to SQL database");
         return;
     }//end createTable
 
     //adapted from https://www.baeldung.com/jdbc-check-table-exists
     private static boolean tableExistsSQL(Connection connection, String tableName) throws SQLException {
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT count(*) "
-            + "FROM information_schema.tables "
-            + "WHERE table_name = ?"
-            + "LIMIT 1;");
-        preparedStatement.setString(1, tableName);
+        DatabaseMetaData meta = connection.getMetaData();
+        ResultSet resultSet = meta.getTables(null, null, tableName, new String[] {"TABLE"});
 
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        return resultSet.getInt(1) != 0;
+        return resultSet.next();
     }
 
 
-    private static String getDataType(Scanner in) {
+    private static String getVariableInformation(Scanner in) {
+        String varName = "";
+        String varType = "";
+        String varLength = "";
+        System.out.println("What would you like the variable to be named?");
+        varName = in.nextLine();
+        System.out.println("What would you like the type of the data to be?");
+        varType = in.nextLine();
+        System.out.println("What would you like the size of the data to be?\n" +
+            "If this datatype does not take size, simply hit enter to skip.");
+        varLength = in.nextLine();
+        if((varLength + varName + varType).equals(""))
+            return "";
 
+        if(varLength.equals("")) {
+            return varName + " " + varType.toUpperCase();
+        } else {
+            if (varLength.contains("(")) {
+                return varName + " " + varType.toUpperCase() + varLength;
+            } else {
+                return varName + " " + varType.toUpperCase() + "(" + varLength + ")";
+            }
+        }
     }
 
 
@@ -172,19 +186,151 @@ public class Database {
 
     //insert into an existing table
     public static void insertEntry(Connection database, Scanner in) throws IOException {
+        boolean keepReading = true;
+        String sqlInsert = "";
+        try {
+            Statement stmt = database.createStatement();
+            //if table already exists, kick user out
+            System.out.print("Enter table name: ");
+            String tableName = in.nextLine();
+            System.out.println();
+            if (!tableExistsSQL(database, tableName)) {
+                System.err.println("Error! cannot add to table " + tableName + " as it does not exist!");
+                return;
+            }
+            ArrayList<String> variables = new ArrayList<String>();
+            //take in and store column info
+            //System.out.println("Reading variables - to continue, simply hit enter on all three fields");
+            DatabaseMetaData metadata = database.getMetaData();
+            ResultSet resultSet = metadata.getColumns(null, null, tableName, null);
+            String varNames = "(";
+            while (resultSet.next()) {
+                String name = resultSet.getString("COLUMN_NAME");
+                String type = resultSet.getString("TYPE_NAME");
+                int size = resultSet.getInt("COLUMN_SIZE");
+                System.out.println("ENTER VALUE FOR " + name + " " + type + "(" + size + "):");
+                String value = in.nextLine();
+                if(type == "VARCHAR" || type == "CHAR") {
+                    value = "\"" + value + "\"";
+                }
+                variables.add(value);
+                varNames += name + ", ";
+            }
+            //build string for create
+            sqlInsert += "INSERT INTO " + tableName + " " + varNames.substring(0, varNames.length()-2) + ") VALUES (";
+            for(int i = 0; i < variables.size()-1; i++) {
+                sqlInsert += variables.get(i) + ", ";
+            }
+            sqlInsert += variables.get(variables.size()-1) + ")";
+
+            //send create statement to the DB
+            stmt.executeUpdate(sqlInsert);
+        } catch (Exception e) {
+            System.err.println("ERROR - error inserting into table");
+            System.err.println(sqlInsert);
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Wrote entry to SQL database");
         return;
     }//end insert
 
 
     //mark db entry for removal
     public static void removeEntry(Connection database, Scanner in) {
-       return;
+        boolean keepReading = true;
+        String sqlRemove = "";
+        try {
+            Statement stmt = database.createStatement();
+            //if table already exists, kick user out
+            System.out.print("Enter table name: ");
+            String tableName = in.nextLine();
+            System.out.println();
+            if (!tableExistsSQL(database, tableName)) {
+                System.err.println("Error! cannot add to table " + tableName + " as it does not exist!");
+                return;
+            }
+            ArrayList<String> variables = new ArrayList<String>();
+            //take in and store column info
+            //System.out.println("Reading variables - to continue, simply hit enter on all three fields");
+            DatabaseMetaData metadata = database.getMetaData();
+            ResultSet resultSet = metadata.getColumns(null, null, tableName, null);
+            System.out.println("Enter name of column you would like to specify a value to delete from");
+            String colToDelete = in.nextLine();
+            while (resultSet.next()) {
+                String name = resultSet.getString("COLUMN_NAME");
+                if(!colToDelete.equals(name))
+                    continue;
+                String type = resultSet.getString("TYPE_NAME");
+                int size = resultSet.getInt("COLUMN_SIZE");
+                System.out.println("ENTER VALUE FOR " + name + " " + type + "(" + size + "):");
+                String value = in.nextLine();
+                if(type == "VARCHAR" || type == "CHAR") {
+                    value = "\"" + value + "\"";
+                }
+                variables.add(value);
+                //varNames += name + ", ";
+            }
+            //build string for create
+            sqlRemove = "DELETE FROM " + tableName + " WHERE " + colToDelete + " = " +  variables.get(0);
+            //send create statement to the DB
+            stmt.executeUpdate(sqlRemove);
+        } catch (Exception e) {
+            System.err.println("ERROR - error inserting into table");
+            System.err.println(sqlRemove);
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Deleted row from SQL database");
+        return;
     }//end remove
 
 
     //print contents of dbfile
     public static void printTableContents(Connection database, Scanner in) {
+        try {
+            Statement stmt = database.createStatement();
+            //if table already exists, kick user out
+            System.out.print("Enter table name: ");
+            String tableName = in.nextLine();
+            System.out.println();
+            if (!tableExistsSQL(database, tableName)) {
+                System.err.println("Error! cannot add to table " + tableName + " as it does not exist!");
+                return;
+            }
+            ArrayList<String> variables = new ArrayList<String>();
+            //take in and store column info
+            //System.out.println("Reading variables - to continue, simply hit enter on all three fields");
+            DatabaseMetaData metadata = database.getMetaData();
+            ResultSet resultSet = stmt.executeQuery( "select * from " + tableName);
+            ResultSetMetaData rsmd = resultSet.getMetaData();
+            int numCol = rsmd.getColumnCount();
 
+            //PRINTING THE TABLE
+            for (int i = 1; i <= numCol; i++) {
+                if (i > 1) System.out.print(",\t");
+                String columnName = rsmd.getColumnName(i);
+                System.out.print(columnName);
+            }
+            System.out.println("");
+            while (resultSet.next()) {
+                for (int i = 1; i <= numCol; i++) {
+                    if (i > 1) System.out.print(",\t");
+                    String columnValue = resultSet.getString(i);
+                    System.out.print(columnValue);
+                }
+                System.out.println("");
+            }
+
+            stmt.close();
+
+        } catch (Exception e) {
+            System.err.println("ERROR - error printing table");
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Deleted row from SQL database");
+        return;
     }//end printFile
 
     private static void returnBook(Connection database, Scanner in) {
